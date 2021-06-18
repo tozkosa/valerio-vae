38248b0b8f6c722fd38c36fb18ac3e2980296908
tozkosa <tomoko.ozeki@gmail.com>
2021-06-18 15:07:28 +0900

    video 6

--- ae.py
+++ ae.py
@@ -2,6 +2,8 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, \
     Flatten, Dense, Reshape, Conv2DTranspose, Activation
 from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MeanSquaredError
 import numpy as np
 
 class Autoencoder:
@@ -24,17 +26,36 @@ class Autoencoder:
 
         self._num_conv_layers = len(conv_filters)
         self._shape_before_bottleneck = None
+        self._model_input = None
 
         self._build()
 
     def summary(self):
         self.encoder.summary()
         self.decoder.summary()
+        self.model.summary()
+
+    def compile(self, learning_rate=0.0001):
+        optimizer = Adam(learning_rate=learning_rate)
+        mse_loss = MeanSquaredError()
+        self.model.compile(optimizer=optimizer, loss=mse_loss)
+
+    def train(self, x_train, batch_size, num_epochs):
+        self.model.fit(x_train,
+                       x_train,
+                       batch_size=batch_size,
+                       epochs=num_epochs,
+                       shuffle=True)
 
     def _build(self):
         self._build_encoder()
         self._build_decoder()
         self._build_autoencoder()
+
+    def _build_autoencoder(self):
+        model_input = self._model_input
+        model_output = self.decoder(self.encoder(model_input))
+        self.model = Model(model_input, model_output, name="autoencoder")
 
     def _build_decoder(self):
         decoder_input = self._add_decoder_input()
@@ -93,6 +114,7 @@ class Autoencoder:
         encoder_input = self._add_encoder_input()
         conv_layers = self._add_conv_layers(encoder_input)
         bottleneck = self._add_bottleneck(conv_layers)
+        self._model_input = encoder_input
         self.encoder = Model(encoder_input, bottleneck, name="encoder")
 
     def _add_encoder_input(self):
@@ -138,6 +160,4 @@ if __name__ == "__main__":
         latent_space_dim=2
     )
     autoencoder.summary()
-
-
 
