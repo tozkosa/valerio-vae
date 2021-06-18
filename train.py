97d8239b5754861b91f37c6e47c04157f74ef5a8
Tomoko Ozeki <tomoko.ozeki@gmail.com>
2021-06-19 08:57:22 +0900

    video 6 finished

--- train.py
+++ train.py
@@ -5,15 +5,17 @@ LEARNING_RATE = 0.0005
 BATCH_SIZE = 32
 EPOCHS = 20
 
+
 def load_mnist():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = x_train.astype("float32") / 255
     x_train = x_train.reshape(x_train.shape + (1,))
-    x_test = x_train.astype("float32") / 255
-    x_test = x_train.reshape(x_train.shape + (1,))
+    x_test = x_test.astype("float32") / 255
+    x_test = x_test.reshape(x_test.shape + (1,))
 
     return x_train, y_train, x_test, y_test
+
 
 def train(x_train, learning_rate, batch_size, epochs):
     autoencoder = Autoencoder(
@@ -29,7 +31,7 @@ def train(x_train, learning_rate, batch_size, epochs):
 
     return autoencoder
 
+
 if __name__ == "__main__":
     x_train, _, _, _ = load_mnist()
     autoencoder = train(x_train[:500], LEARNING_RATE, BATCH_SIZE, EPOCHS)
-
